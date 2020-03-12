dc22773eab619c2535411575d7c1119dc7f6b264
tramvtn <tramvtn@runsystem.net>
2020-03-12 09:03:45 +0700

    up main js

--- luxury/assets/js/main.js
+++ luxury/assets/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
 	$(".js-change-background").on("click", function(e) {
 		$(this).toggleClass('active');
 	});
+	$(".js-change-background").on("change", function(e) {
+		$(this).toggleClass('active');
+	});
 	$("body").on("click", function(e) {
 		if(!$(e.target).is("select")) {
 			if($(".js-change-background").hasClass('active')) {
