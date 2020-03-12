e90c15b98e5ac0107438eec29054a0d94b847eeb
tramvtn <tramvtn@runsystem.net>
2020-03-12 09:29:19 +0700

    up main js

--- luxury/assets/js/main.js
+++ luxury/assets/js/main.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
 	});
 	$(".js-change-background").on("change", function(e) {
 		$(this).removeClass('active');
-		console.log("open select");
 	});
 	$("body").on("click", function(e) {
 		if(!$(e.target).is("select")) {
