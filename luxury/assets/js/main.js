77a2c3cf9a2eb7ad8f02c21aff525827cc775745
tramvtn <tramvtn@runsystem.net>
2020-03-16 17:54:20 +0700

    up fix bug

--- luxury/assets/js/main.js
+++ luxury/assets/js/main.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
-	$(".js-change-background").on("click", function(e) {
-		$(this).toggleClass('active');
-	});
-	$(".js-change-background").on("change", function(e) {
-		if(!$(this).is("select")) {
-			if($(".js-change-background").hasClass('active')) {
-				$(".js-change-background").removeClass('active');
+	$(".js-change-background").each(function(){
+		//click select
+		$(this).click(function(){
+			$(this).parent().parent().siblings().find('.js-change-background').removeClass("active");
+			$(this).toggleClass('active');
+		});
+		// select change value
+		$(".js-change-background").on("change", function(e) {
+			if(!$(this).is("select")) {
+				if($(".js-change-background").hasClass('active')) {
+					$(".js-change-background").removeClass('active');
+				}
 			}
-		}
+		});
+		
 	});
+	//click body change select
 	$("body").on("click", function(e) {
 		if(!$(e.target).is("select")) {
-			if($(".js-change-background").hasClass('active')) {
-				$(".js-change-background").removeClass('active');
+			if($(".js-change-background").hasClass("active")) {
+				$(".js-change-background").removeClass("active");
 			}
 		}
 	});
