c0ec642b6eec2a9e9a817626f55b9f27eb731753
tramvtn <tramvtn@runsystem.net>
2020-03-23 08:40:45 +0700

    up fix js button top

--- ifv/assets/js/main.js
+++ ifv/assets/js/main.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
-  $(".js-menu").each(function(){
-    $(this).find(".js-menu-icn").click(function(){
-			$(this).siblings(".js-menu-content").slideToggle(300);
-			$(this).toggleClass("open");
-		})
-	});
-
-	// scroll top button
-	$('.js-scrollToTop').on('click',function(){
-		var targetScroll = 0;
-		$("html,body").animate({
-			scrollTop : targetScroll
-		}, 300,'linear', function() {
-		});
-	});
-
+	$(".js-menu").each(function(){
+	  $(this).find(".js-menu-icn").click(function(){
+			  $(this).siblings(".js-menu-content").slideToggle(300);
+			  $(this).toggleClass("open");
+		  })
+	  });
+  
+	  // scroll top button
+	  $('.js-scrollToTop').on('click',function(){
+		  var targetScroll = 0;
+		  $("html,body").animate({
+			  scrollTop : targetScroll
+		  }, 300,'linear', function() {
+		  });
+	  });
+	  $(window).scroll(function() {
+	  if ($(this).scrollTop() > 100) {
+		  $(".icn-arrow-top").fadeIn();
+	  } else {
+		  $(".icn-arrow-top").fadeOut();
+	  }
+  });
+  
 });
